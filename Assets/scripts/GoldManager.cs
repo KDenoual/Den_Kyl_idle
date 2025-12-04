41c9bc2b01fc3402ac5f944be622912bb1ff9942
KDenoual <k.denoual@rns.ecolescreatives.com>
2025-12-04 13:29:06 +0100

    v2

--- Assets/scripts/GoldManager.cs
+++ Assets/scripts/GoldManager.cs
@@ -5,12 +5,16 @@ public class GoldManager : MonoBehaviour
 {
     public int goldAmount;
     public int power;
+    public int powerPrice;
     public TextMeshProUGUI goldText;
+    public TextMeshProUGUI powerText;
+    public TextMeshProUGUI powerPriceText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         power = 1;
+        powerPrice = 10;
     }
 
     public void ChangeGold()
@@ -21,10 +25,13 @@ public class GoldManager : MonoBehaviour
 
     public void ChangePower()
     {
-        if (goldAmount > 10)
+        if (goldAmount >= powerPrice
+)
         {
-            goldAmount -= 10;
+            goldAmount -= powerPrice;
+            powerPrice = Mathf.CeilToInt(powerPrice * 2f);
             goldText.text = goldAmount.ToString("00");
+            powerText.text = goldAmount.ToString("00");
 
             power += 1;
         }
@@ -32,6 +39,7 @@ public class GoldManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        powerText.text = power.ToString("00");
+        powerPriceText.text = powerPrice.ToString("00");
     }
 }
