c8d4cb70cea3b8411ea668fc4ca02d3b5a15ccc4
KDenoual <k.denoual@rns.ecolescreatives.com>
2025-12-18 17:34:58 +0100

    v0.4

--- Assets/scripts/GoldManager.cs
+++ Assets/scripts/GoldManager.cs
@@ -7,11 +7,15 @@ using UnityEngine.UIElements;
 public class GoldManager : MonoBehaviour
 {
     public int goldAmount;
+    public int goldGain;
+    public int goldGainPrice;
     public int power;
     public int timeGold;
     public int timeGoldPrice;
     public int timeGoldUP;
     public int timeGoldUPPrice;
+    public int timeGoldUP2;
+    public int timeGoldUP2Price;
     public int powerPrice;
     public TextMeshProUGUI goldText;
     public TextMeshProUGUI powerText;
@@ -19,31 +23,40 @@ public class GoldManager : MonoBehaviour
     public TextMeshProUGUI timeGoldPriceText;
     public TextMeshProUGUI timeGoldUPText;
     public TextMeshProUGUI timeGoldUPPriceText;
+    public TextMeshProUGUI timeGoldUP2Text;
+    public TextMeshProUGUI timeGoldUP2PriceText;
+    public TextMeshProUGUI goldChangeText;
+    public TextMeshProUGUI goldChangePriceText;
     public TextMeshProUGUI powerPriceText;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        goldGain = 1;
+        goldGainPrice = 50000;
         power = 0;
         powerPrice = 25;
         timeGold = 0;
         timeGoldPrice = 100;
         timeGoldUP = 0;
         timeGoldUPPrice = 1000;
+        timeGoldUP2 = 0;
+        timeGoldUP2Price = 10000;
         StartCoroutine(autoGold());
         StartCoroutine(autoGoldUP());
+        StartCoroutine(autoGoldUP2());
     }
 
     public void ChangeGold()
     {
-        goldAmount += power + 1;
+        goldAmount += power + goldGain;
     }
     public void LaunchCoroutine()
     {
         if (goldAmount >= timeGoldPrice)
         {
             goldAmount -= timeGoldPrice;
-            timeGoldPrice = Mathf.CeilToInt(timeGoldPrice * 1.2f);
+            timeGoldPrice = Mathf.CeilToInt(timeGoldPrice * 1.5f);
 
             timeGold += 5;
         }
@@ -53,9 +66,19 @@ public class GoldManager : MonoBehaviour
         if (goldAmount >= timeGoldUPPrice)
         {
             goldAmount -= timeGoldUPPrice;
-            timeGoldUPPrice = Mathf.CeilToInt(timeGoldUPPrice * 1.2f);
+            timeGoldUPPrice = Mathf.CeilToInt(timeGoldUPPrice * 1.5f);
 
-            timeGoldUP += 15;
+            timeGoldUP += 50;
+        }
+    }
+    public void LaunchCoroutineUP2()
+    {
+        if (goldAmount >= timeGoldUP2Price)
+        {
+            goldAmount -= timeGoldUP2Price;
+            timeGoldUP2Price = Mathf.CeilToInt(timeGoldUP2Price * 1.5f);
+
+            timeGoldUP2 += 100;
         }
     }
 
@@ -65,9 +88,20 @@ public class GoldManager : MonoBehaviour
 )
         {
             goldAmount -= powerPrice;
-            powerPrice = Mathf.CeilToInt(powerPrice * 1.2f);
+            powerPrice = Mathf.CeilToInt(powerPrice * 1.5f);
 
             power += 1;
+        }
+    }
+
+    public void ChangeGoldUP()
+    {
+        if (goldAmount >= goldGainPrice
+)
+        {
+            goldAmount -= goldGainPrice;
+
+            goldGain += 9;
         }
     }
     // Update is called once per frame
@@ -79,22 +113,34 @@ public class GoldManager : MonoBehaviour
         timeGoldText.text = timeGold.ToString("00");
         timeGoldUPPriceText.text = timeGoldUPPrice.ToString("00");
         timeGoldUPText.text = timeGoldUP.ToString("00");
+        timeGoldUP2PriceText.text = timeGoldUP2Price.ToString("00");
+        timeGoldUP2Text.text = timeGoldUP2.ToString("00");
+        goldChangePriceText.text = goldGainPrice.ToString("00");
+        goldChangeText.text = goldGain.ToString("00");
         goldText.text = goldAmount.ToString("00");
     }
     public IEnumerator autoGold()
     {
             while (true)
             {    
-                yield return new WaitForSeconds(10);
-                goldAmount += timeGold;
+                yield return new WaitForSeconds(5);
+                goldAmount += timeGold * goldGain;
             }
     }
     public IEnumerator autoGoldUP()
     {
             while (true)
             {
+            yield return new WaitForSeconds(3);
+                goldAmount += timeGoldUP * goldGain;
+            }
+    }
+    public IEnumerator autoGoldUP2()
+    {
+            while (true)
+            {
             yield return new WaitForSeconds(1);
-                goldAmount += timeGoldUP;
+                goldAmount += timeGoldUP2 * goldGain;
             }
     }
 
